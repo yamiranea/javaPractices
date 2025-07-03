1bc06aa98465523c92708dd29b581a4bca980aa8
YamilaRanea <yamilaranea@gmail.com>
2025-07-03 11:56:48 +0200

    First commit

---
+++ Arithmetic/src/arithmetic/Arithmetic.java
@@ -0,0 +1,17 @@
+package arithmetic;
+
+public class Arithmetic {
+
+	public static void main(String[] args) 
+	{
+		//example values
+		float a=14.3f,b=3.2f;
+		
+		float r=a%b;
+		
+		System.out.println(r);
+		
+		
+	}
+
+}
