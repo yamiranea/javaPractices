96ca5defa30efecfbe5fdbaa13605a1f9296d56c
YamilaRanea <yamilaranea@gmail.com>
2025-07-11 10:56:16 +0200

    Creating SCCondition3 logic

--- scconditions3/src/scconditions3/SCCondition3.java
+++ scconditions3/src/scconditions3/SCCondition3.java
@@ -1,9 +1,16 @@
 package scconditions3;
 
+import java.util.Scanner;
+
 public class SCCondition3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		
+		System.out.println("Enter a URL");
+		String url= scan.nextLine();
+		
+		String protocol=url.substring(0, 0);
 
 	}
 
